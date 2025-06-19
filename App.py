384de418b26f8892bc52a761f271fe3c3521cadd
Parixit <jagadaparik@gmail.com>
2025-06-19 13:10:30 +0530

    remove dotenv

--- App.py
+++ App.py
@@ -1,9 +1,7 @@
 import streamlit as st
-from dotenv import load_dotenv
 import os
 from api_calls import get_response
 
-load_dotenv()
 api_key = os.getenv('API_KEY')
 
 st.set_page_config(page_title="DigiBuddy Chatbot", page_icon="💬", layout="centered")
